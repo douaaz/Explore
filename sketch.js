95753e806f7f09a0b40412a625683e80e49243fe
douaa <dz2238@nyu.edu>
2021-12-03 12:38:47 +0100

    please work

--- sketch.js
+++ sketch.js
@@ -1,17 +1,12 @@
 let w=30;
-let b=0;
-let h=0;
-let img;
-function preload(){
-  img=loadImage('assets/worldmap.jpg');
-}
+
 function setup() {
   createCanvas(400, 400, WEBGL);
-  image(img,0,0,b,h);
+
 }
 
 function draw() {
-  
+
   background(0, 0, 70);
   push();
   rotateY(millis() / 1000);
@@ -20,13 +15,5 @@ function draw() {
   sphere(w, 16, 17);
   pop();
   if (mouseIsPressed) {
-    w=w+1 
-    if (w>300) {
-      w=0;
-      b=400;
-      h=400;
-      
-    }
-  }
+    w=w+1  }
 }
-
