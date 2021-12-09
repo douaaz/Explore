0833a1441cf9d97dc0dd74839ede844eaa1ff0fe
douaa <dz2238@nyu.edu>
2021-12-09 02:40:22 +0100

    attempt

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let  largebox= document.querySelector('#largebox');
+largebox.style.display='none'
+let openbutton= document.querySelector('#openbutton');
+let dtbox= document.querySelector('#dtbox');
+dtbox.style.display='none'
+let opendtbutton= document.querySelector('#opendtbutton');
+
+function before() {
+ document.getElementById('pic1')
+  .src='https://voguishcar.com/wp-content/uploads/2020/02/La-mosquee-Hassan-II.jpg';
+}
+function after(){
+  document.getElementById('pic1')
+  .src='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2857650.jpg';
+}
+function dtbefore() {
+  document.getElementById('dtpic1')
+  .src='https://i.pinimg.com/564x/40/7a/6b/407a6b1125aac294c7f84f1434f6678a.jpg'
+}
+function dtafter() {
+  document.getElementById('dtpic1')
+  .src='https://i.pinimg.com/564x/3f/dd/75/3fdd7504ee82fba5fc53249b9532cd99.jpg'
+}
+function closebox() {
+ largebox.style.display='none'
+ openbutton.style.display='inline'
+ opendtbutton.style.display='inline'
+
+}
+function closedtbox() {
+ dtbox.style.display='none'
+ opendtbutton.style.display='inline'
+ openbutton.style.display='inline'
+
+}
+function openbox() {
+ largebox.style.display='inline'
+  openbutton.style.display='none'
+  opendtbutton.style.display='none'
+}
+function opendtbox() {
+  dtbox.style.display='inline'
+  opendtbutton.style.display='none'
+  openbutton.style.display='none'
+}
+
+
+var song=document.getElementById('song');
+var icon=document.getElementById('picplay');
+icon.onclick = function() {
+  if (song.paused){
+    song.play();
+    icon.src = "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX26341460.jpg"
+  } else{
+    song.pause();
+    icon.src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c4f5.png"
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,19 +1,32 @@
-let w=30;
+var canvas;
+let w = 30;
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
-
+  canvas = createCanvas(1300, 580, WEBGL);
+  canvas.position(0, 0);
 }
 
 function draw() {
-
-  background(0, 0, 70);
+  background(0, 0, 20);
   push();
   rotateY(millis() / 1000);
   stroke(0, 0, 149);
-  fill(0, 0, 179);
-  sphere(w, 16, 17);
+  fill(0, 46, 144);
+  sphere(w, 18, 17);
+  fill(0, 150, 0);
+  stroke(0, 100, 0);
+  sphere(w, 23, 7);
+  fill(255, 255, 255, 20);
+  noStroke();
+  sphere(w, 20, 20);
   pop();
-  if (mouseIsPressed) {
-    w=w+1  }
+
+ if (mouseIsPressed) {
+    w = w + 1;
+  }
+  if (w>80){
+    remove();
+  }
+
+
 }
